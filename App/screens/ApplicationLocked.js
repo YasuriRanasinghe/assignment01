ea928356529da4ebe94fe16c8cbf70eeff947990
YasuriRanasinghe <ar97984@fhss.sjp.ac.lk>
2022-08-10 21:14:46 +0530

    initial commit

---
+++ App/screens/ApplicationLocked.js
@@ -0,0 +1,132 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+  Image,
+} from 'react-native';
+
+export default class ApplicationLocked extends Component {
+  constructor() {
+    super();
+    this.state = {
+      seconds: 60,
+      minutes: 4,
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({seconds: this.state.seconds - 1});
+      if (this.seconds == 0) {
+        this.setState({minutes: this.state.minutes - 1});
+      }
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View
+          style={{
+            marginTop: 100,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View>
+            <Text style={styles.Text}> Maximum attempts reached</Text>
+          </View>
+          <View style={styles.Time}>
+            <Text style={styles.Text4}>
+              {this.state.minutes} : {this.state.seconds}
+            </Text>
+          </View>
+          <View style={styles.image}>
+            <Image source={require('../assets/image/Icon/LOCK.png')} />
+          </View>
+          <View>
+            <Text style={styles.Text2}>
+              To protect your information, access has been locked for 5 minutes.
+              Come back later and try again.
+            </Text>
+          </View>
+          <TouchableOpacity onPress={() => this}>
+            <View style={styles.Text3View}>
+              <Text style={styles.Text3}> Quit</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  Text: {
+    fontFamily: 'Roboto-Light',
+    fontSize: 25,
+    color: '#92969f',
+    letterSpacing: 0.34,
+    lineHeight: 25,
+    marginBottom: 50,
+  },
+
+  image: {
+    width: 32,
+    height: 35,
+    marginTop: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Text2: {
+    marginTop: 80,
+    fontFamily: 'Roboto-Light',
+    fontSize: 22,
+    color: '#92969f',
+    letterSpacing: 0.34,
+    lineHeight: 25,
+    justifyContent: 'flex-end',
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: 'center',
+  },
+  Text3View: {
+    backgroundColor: '#a6dde0',
+    width: 100,
+    height: 60,
+    marginTop: 150,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  Text3: {
+    fontFamily: 'Roboto-Light',
+    fontSize: 22,
+    marginLeft: 20,
+  },
+  Text4: {
+    fontSize: 20,
+    color: '#313334',
+    textAlign: 'center',
+    justifyContent: 'center',
+    fontFamily: 'Roboto-Light',
+    padding: 8,
+    color: 'red',
+    height: 40,
+    width: 100,
+  },
+  Time: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderColor: 'gray',
+    textShadowRadius: 10,
+    borderWidth: 3,
+  },
+});
---
+++ App/screens/LockScreenPasscode.js
@@ -0,0 +1,211 @@
+import React, {Component} from 'react';
+import {
+  TouchableOpacity,
+  StatusBar,
+  SafeAreaView,
+  TouchableHighlight,
+  Image,
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+} from 'react-native';
+
+class LockScreenPasscode extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passcode: ['', '', '', ''],
+    };
+  }
+  _popUpMsg = () => {
+    let text = this.state.passcode.toString('1', '2', '3', '4');
+    if (text === '1, 2, 3, 4') {
+      Alert.alert('Success!..', ' App will redirect you to the home now!', [
+        {text: 'ok.'},
+      ]);
+    } else {
+      for (var i = 1; i <= 3; i++) {
+        if ((text !== '1', '2', '3', '4')) {
+          Alert.alert('Retry!..', ' Please enter your PIN Again!', [
+            {text: 'ok.'},
+          ]);
+        }
+      }
+    }
+  };
+
+  _onPressNumber = num => {
+    let tempCode = this.state.passcode;
+    for (var i = 0; i < tempCode.length; i++) {
+      if (tempCode[i] === '') {
+        tempCode[i] = num - -1;
+        break;
+      } else {
+        continue;
+      }
+    }
+    this.setState({passcode: tempCode});
+  };
+
+  _onPressCancel = () => {
+    let tempCode = this.state.passcode;
+    for (var i = tempCode.length - 1; i >= 0; i--) {
+      if (tempCode[i] !== '') {
+        tempCode[i] = '';
+        break;
+      } else {
+        continue;
+      }
+    }
+    this.setState({passcode: tempCode});
+  };
+  render() {
+    let numbers = [
+      {id: 1},
+      {id: 2},
+      {id: 3},
+      {id: 4},
+      {id: 5},
+      {id: 6},
+      {id: 7},
+      {id: 8},
+      {id: 9},
+      {id: 0},
+    ];
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <View
+          style={{
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <View>
+            <Text style={styles.Text}> Enter your PIN Code</Text>
+          </View>
+          <View style={styles.codeContainer}>
+            {this.state.passcode.map(p => {
+              let style = p !== '' ? styles.code2 : styles.code1;
+              return <View style={style} />;
+            })}
+          </View>
+        </View>
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <View style={styles.numberContainer}>
+            {numbers.map(num => {
+              return (
+                <TouchableHighlight
+                  style={styles.number}
+                  key={num.id}
+                  underlayColor="#6ec8c9"
+                  onPress={() => this._onPressNumber(num.id)}>
+                  <Text style={styles.numText}> {num.id} </Text>
+                </TouchableHighlight>
+              );
+            })}
+            <View style={styles.buttons}>
+              <TouchableOpacity onPress={() => this._onPressCancel()}>
+                <Image source={require('../assets/image/Icon/ICON.png')} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.number2}
+                onPress={() => this._popUpMsg()}>
+                <Text style={styles.numText2}>OK</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+export default LockScreenPasscode;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'space-around',
+  },
+  Text: {
+    marginTop: 10,
+    fontFamily: 'Roboto-Light',
+    fontSize: 25,
+    color: '#92969f',
+    letterSpacing: 0.34,
+    lineHeight: 25,
+  },
+  codeContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 145,
+    flexDirection: 'row',
+    marginBottom: -150,
+  },
+  code1: {
+    margin: 8,
+    width: 10,
+    height: 12,
+    borderRadius: 13,
+    borderWidth: 2,
+    borderColor: '#1bddf2',
+    backgroundColor: '#1bddf2',
+  },
+
+  code2: {
+    margin: 8,
+    width: 12,
+    height: 14,
+    borderRadius: 13,
+    borderWidth: 2,
+    borderColor: '#0ba39c',
+    backgroundColor: '#0ba39c',
+  },
+  number: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 75,
+    height: 75,
+    margin: 8,
+    borderRadius: 75,
+    backgroundColor: '#f0f3fa',
+  },
+  numberContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: '#0a191a',
+    marginTop: 58,
+    width: 282,
+    height: 348,
+  },
+  numText: {
+    color: '#92969f',
+    textAlign: 'center',
+    fontFamily: 'Roboto-Light',
+    fontSize: 38,
+  },
+  buttons: {
+    alignItems: 'center',
+    marginRight: -65,
+  },
+  numText2: {
+    color: '#92969f',
+    fontFamily: 'Roboto-Light',
+    fontSize: 20,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  number2: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 75,
+    height: 75,
+    marginTop: -60,
+    marginLeft: -340,
+    borderRadius: 75,
+    backgroundColor: '#f0f3fa',
+  },
+});
